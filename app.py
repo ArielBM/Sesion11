a4794496399de97a82cc9474f3e3a717e52f1685
ArielBM <201503910@ingenieria.usac.edu.gt>
2021-04-08 10:07:12 -0600

    Ruta Saludar
    Se implementó la función para saludar en la ruta: "/saludar"

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ CORS(app)
 def index():
     return "<H1>BIENVENIDOS ESTUDIANTES DE IPC1</H1>"
 
+
+@app.route("/saludar")
+def saludar():
+    return "<H1>HOLA</H1>"
+
 if __name__ == "__main__":
     app.run(threaded=True, port=5001, debug=True)
 
