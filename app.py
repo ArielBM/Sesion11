c0e8ce1b4be0da740282d1e5a64510b5e12b04e3
ArielBM <201503910@ingenieria.usac.edu.gt>
2021-04-08 11:08:23 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from datetime import datetime as dt
 from flask import Flask
 from flask_cors import CORS
+from models.usuario import usuario
+
+usuario_admin = usuario(0,"Ariel","abautista","1234",1)
 
 app = Flask(__name__)
 CORS(app)
@@ -19,6 +22,11 @@ def saludar():
 def datetime():
     now_utc = dt.now()
     return "<H1>" + str(now_utc) + "</H1>"
+
+
+@app.route("/who-is-admin")
+def datetime():
+    return "<H1>" + usuario_admin.nombre + " is admin</H1>"
 
 if __name__ == "__main__":
     app.run(threaded=True, port=5001, debug=True)
